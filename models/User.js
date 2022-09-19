5ad1cbaaa1cf902428bffe1870b0efa15446dcc5
ezravkatz <ezravkatz@gmail.com>
2022-09-19 12:11:46 -0400

    CRUD API routes created

--- models/User.js
+++ models/User.js
@@ -9,13 +9,9 @@ User.init(
   {
     // define an id column
     id: {
-      // use the special Sequelize DataTypes object provide what type of data it is
       type: DataTypes.INTEGER,
-      // this is the equivalent of SQL's `NOT NULL` option
       allowNull: false,
-      // instruct that this is the Primary Key
       primaryKey: true,
-      // turn on auto increment
       autoIncrement: true
     },
     // define a username column
@@ -27,9 +23,7 @@ User.init(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // there cannot be any duplicate email values in this table
       unique: true,
-      // if allowNull is set to false, we can run our data through validators before creating the table data
       validate: {
         isEmail: true
       }
@@ -39,7 +33,6 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least four characters long
         len: [4]
       }
     }
